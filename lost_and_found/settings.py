57a95313bafb9509ccb2ba189a677397c027077c
Divad <ezoodavidgael@gmail.com>
2025-02-16 03:39:12 +0100

    feat(ui): integrate Jazzmin for Django admin customization

--- lost_and_found/settings.py
+++ lost_and_found/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
@@ -121,3 +122,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+JAZZMIN_SETTINGS = {
+    "site_title": "LF Admin",
+    "site_header": "LF Admin",
+    "site_brand": "LF Admin",
+    "welcome_sign": "Bienvenue dans l'interface d'administration de LF",
+}
